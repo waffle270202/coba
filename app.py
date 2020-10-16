6f8cbfe3ba09cb9deb9a83c3dbd6c1792aa9214b
waffle270202 <72966311+waffle270202@users.noreply.github.com>
2020-10-16 17:45:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
 def handle_message(event):
     if msg_from_user == 'a':
         message = ("hello world")
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(message)
    
 
 import os

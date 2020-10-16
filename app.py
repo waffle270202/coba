43e1d0ab85f6cadc67da54fa47c222095895e7ee
waffle270202 <72966311+waffle270202@users.noreply.github.com>
2020-10-16 17:30:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,10 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    if msg_from_user == 'a':
+        message = "hello world"
+        line_bot_api.reply_message(event.reply_token, message)
+   
 
 import os
 if __name__ == "__main__":
